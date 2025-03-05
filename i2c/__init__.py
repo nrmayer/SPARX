cef8d62d2b0cb8695913c0157791f723f2eda17c
Noah <noahrmayer@gmail.com>
2025-03-05 11:34:44 -0700

    i2c typing

---
+++ i2c/__init__.py
@@ -0,0 +1,10 @@
+"""
+I2C handler module. Taken and modified from Adafruit libraries
+
+
+i2c_device.py from adafruit-circutpython-busdevice-5.2.11 (imports slightly modified)
+
+i2c_bit.py, i2c_bits.py, i2c_struct.py from adafruit-circutpython-register-1.10.2 (imports and class functions slightly modified)
+"""
+
+# empty
