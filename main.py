10c6b8e0a1dd16f172506f8e193377779e7a1e80
nrmayer <nrmayer01@bvsd.org>
2026-03-09 17:07:49 -0600

    Temperature logging and print logging in `main.py`

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pins
-# from log import init_global_logger, global_logger
+from log import init_global_logger, global_logger
 
+PRINT_LOGGING = True
 
 def main() -> None:
     running = False
     last = False
+    last_T = 0.0
 
-    # global_logger().write_info("start main()")
+    global_logger().write_info("start main()")
     while True:
-        if pins.THERMOCOUPLE_ENALED:
-            print(pins.thermocouple.temperature)
+        T = pins.thermocouple.temperature
 
+        # if temperature has changed enough, write log entry
+        if abs(T-last_T) > 0.5:
+            global_logger().write_info(f"T: {T:.2f}°C")
+            last_T = T
+
+        if PRINT_LOGGING: 
+            print(f"T: {T:.2f}°C", end ='\r')
+            sleep(0.5)
+        
         running = bool(pins.run_pin.state)
         if last == running: continue
+
+        global_logger().write_info(f"set state to {running}")
+        if PRINT_LOGGING: print(f"Set state to {running}")
 
         pins.valve.set(running)
         pins.mosfet.set(running)
@@ -20,12 +33,12 @@ def main() -> None:
         last = running
 
 if __name__ == "__main__":
-    # init_global_logger(folder="logs/")
+    init_global_logger(folder="logs/", print_log=PRINT_LOGGING)
     try:
         main()
     except Exception as e:
-        # global_logger().write_error(f"System exit with exception {e}")
+        global_logger().write_error(f"System exit with exception {e}")
         pass
     finally:
-        # global_logger().write_info("System exit")
+        global_logger().write_info("System exit")
         pins.board.reset()
